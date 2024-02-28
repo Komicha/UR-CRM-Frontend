309b9e37f0e7c204918edd302f30d3b2559557a7
Koaul <ohapckin.andrej@yandex.ru>
2024-02-28 23:27:51 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import { Fragment } from "react/jsx-runtime";
 import {
   createBrowserRouter,
   BrowserRouter,
   Route,
   Routes,
 } from "react-router-dom";
-import GlobalStyles from "./theme/globalStyle";
 
 import Main from "./pages/main";
 import ProjectTasks from "./pages/project-tasks";
 
 function App() {
   return (
-    <Fragment>
-      <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/project-tasks" element={<ProjectTasks />} />
-        </Routes>
-      </BrowserRouter>
-    </Fragment>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/project-tasks" element={<ProjectTasks />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import GlobalStyles from "theme/globalStyle";
+import { Normalize } from "styled-normalize";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Normalize />
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
