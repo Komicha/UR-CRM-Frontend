d391e7750079b8ad1e0fb2c068bfb423beb8790d
mehanik314 <vihr76@yandex.ru>
2024-02-28 22:22:20 +0300

    app

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import {
+    createBrowserRouter,
+    BrowserRouter,
+    Route,
+    Routes,
+} from "react-router-dom";
+
+function App() {
+
+    return (
+
+        <BrowserRouter>
+            <Routes>
+
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ src/pages/user-selection/index.tsx
@@ -0,0 +1,5 @@
+const UserSelection = () => {
+    return (<div></div>);
+}
+
+export default UserSelection;
