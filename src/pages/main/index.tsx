5ad7a3922a0943b96f58a4afb5c6488c44b5ea54
mehanik314 <vihr76@yandex.ru>
2024-03-02 17:29:15 +0300

    test api

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -35,10 +35,11 @@ const StyledName = styled.p`
 const Main = () => {
     tg.BackButton.show();
     const name = tg.initDataUnsafe?.user?.username
-    const imgprofile = tg.initDataUnsafe.user?.photo_url
-
+    const imgprofile = tg.initDataUnsafe?.user?.photo_url
+    const chattype = tg.initDataUnsafe?.chat?.type
     console.log("пользователь: " + name)
     console.log("url photo: " + imgprofile)
+    console.log("тип чата: " + chattype)
     return (
         <StyledMain>
             <StyledAccount>
@@ -47,6 +48,7 @@ const Main = () => {
                 </StyledPhoto>
                 <StyledName>
                     {name}
+
                 </StyledName>
                 <svg width="9" height="16" viewBox="0 0 9 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M8.70711 8.70711C9.09763 8.31658 9.09763 7.68342 8.70711 7.29289L2.34315 0.928932C1.95262 0.538408 1.31946 0.538408 0.928932 0.928932C0.538408 1.31946 0.538408 1.95262 0.928932 2.34315L6.58579 8L0.928932 13.6569C0.538408 14.0474 0.538408 14.6805 0.928932 15.0711C1.31946 15.4616 1.95262 15.4616 2.34315 15.0711L8.70711 8.70711ZM7 9H8V7H7V9Z" fill="white" />
